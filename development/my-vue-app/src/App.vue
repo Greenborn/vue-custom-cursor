b830f987859c3a86693c88fda83f468bacdcbaf8
luciano <lucho.2012.tandil@gmail.com>
2024-12-11 19:26:28 -0300

    Se agrega directorio de proyecto de desarrollo de componente, se oculta cursor pointer en botones y enlaces

---
+++ development/my-vue-app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import CustomCursor from './components/CustomCursor.vue'
+</script>
+
+<template>
+  <CustomCursor />
+
+  <a href="https://github.com/Greenborn/vue-custom-cursor"> GitHub</a>
+  <button>Botón</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ development/my-vue-app/src/components/CustomCursor.vue
@@ -0,0 +1,63 @@
+<template>
+  <div v-if="cursor_show" class="inverter-cursor" :class="{ 'resaltar-cursor': resaltar }" :style="estilo"></div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  
+  const estilo = ref({ left: "555px", top: "570.033px", x_offset: -15, y_offset: -15 })
+  const cursor_show = ref(false)
+  const resaltar = ref(false)
+  const ultimo_tmp_a = ref(0)
+  
+  onMounted(()=>{
+      document.addEventListener('mousemove', (e) => {
+          estilo.value.left = (e.clientX + estilo.value.x_offset) + 'px'
+          estilo.value.top = (e.clientY + estilo.value.y_offset) + 'px'
+          cursor_show.value = true
+  
+          if (new Date().getTime() - ultimo_tmp_a.value > 200)
+              resaltar.value = false
+      })
+  
+      document.querySelectorAll('a').forEach(function(enlace) {
+          resaltados(enlace)
+      })
+  
+      document.querySelectorAll('button').forEach(function(enlace) {
+          resaltados(enlace)
+      })
+  })
+  
+  function resaltados(obj){
+      return obj.addEventListener('mousemove', function(e) {
+          resaltar.value = true
+          ultimo_tmp_a.value = new Date().getTime()
+          estilo.value.left = (e.clientX + estilo.value.x_offset*2) + 'px'
+          estilo.value.top = (e.clientY + estilo.value.y_offset*2) + 'px'
+      })
+  }
+</script>
+  
+<style>
+.inverter-cursor{
+    position: fixed;
+    border-radius: 50%;
+    background-color: white;
+    mix-blend-mode: difference;
+    transition: transform 0.25s ease,top 0.2s ease-out,left 0.2s ease-out, width .2s, height .2s;
+    width: 20px;
+    height: 20px;
+    z-index: 5000;
+    pointer-events: none;
+}
+
+.resaltar-cursor{
+    width: 40px !important;
+    height: 40px !important;
+}
+
+html, body, button, a{
+  cursor: none !important;
+}
+</style>
